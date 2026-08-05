5a58a44f25be7b6eb14a21e1f55d0c00a9acf584
Abhishek Singh <singh.abhishek151019@gmail.com>
2026-08-05 23:46:10 +0530

    Refactor: Comment out initial variable declarations and print statements in 03_numbers.py

--- python/03_numbers.py
+++ python/03_numbers.py
@@ -1,5 +1,74 @@
-age = 25
-temperature = -10
-population = 1400000000
+# age = 25
+# temperature = -10
+# population = 1400000000
+# print(type(age))
 
-print(type(age))
+# x = 999999999999999999999999999999999999
+# print(type(x))
+
+# price = 99.99
+# pi = 3.14159265
+# accuracy = 99.87
+# print(type(price))
+# print(type(pi))
+# print(type(accuracy))
+
+# x = 1e3
+# print(x)
+
+# distance = 3.5e8
+# print(distance)
+
+# z = 3 + 4j
+# print(z)
+# print(z.real)
+# print(z.imag)
+
+# a = 10
+# b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+
+# x = 10
+# x -= 2
+# print(x)
+# x *= 3
+# print(x)
+# x /= 2
+# print(x)
+# x //= 2
+# print(x)
+# x %= 3
+# print(x)
+# x **= 2
+# print(x)
+
+# print(10 > 5)
+# print(10 != 5)
+# print(5 == 5)
+
+# print(abs(-10))
+# print(round(3.14159, 2))
+
+# import math
+# print(math.ceil(3.14159))
+# print(math.floor(3.14159))
+# print(math.sqrt(16))
+# print(math.pow(2, 3))
+# print(math.log(10, 10))
+# print(math.log10(10))
+# print(math.log2(10))
+# print(math.sin(math.pi / 2))
+# print(math.cos(0))
+# print(math.tan(math.pi / 4))
+
+import random
+print(random.randint(1, 10)) # 1 to 10
+print(random.randrange(1, 10)) # 1 to 9
+print(random.random()) # 0.0 to 1.0
