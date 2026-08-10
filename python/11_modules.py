289bae906f559598eba43977d7fe8949ec433c38
Abhishek Singh <singh.abhishek151019@gmail.com>
2026-08-10 14:44:07 +0530

    Refactor: Add math utility functions for basic arithmetic operations and create .gitignore for __pycache__

---
+++ python/11_modules.py
@@ -0,0 +1,19 @@
+# import math_utils
+# print(math_utils.add(10, 20))
+# print(math_utils.subtract(10, 20))
+# print(math_utils.multiply(10, 20))
+# print(math_utils.divide(10, 20))
+
+# import math_utils as mu
+# print(mu.add(10, 20))
+# print(mu.subtract(10, 20))
+# print(mu.multiply(10, 20))
+# print(mu.divide(10, 20))
+
+# from math_utils import add, subtract, multiply, divide
+# print(add(10, 20))
+# print(subtract(10, 20))
+# print(multiply(10, 20))
+# print(divide(10, 20))
+
+print(__name__)  # This will print the name of the module when run directly
