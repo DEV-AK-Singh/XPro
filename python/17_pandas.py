90eee9db690132eb4e8eaf8392ec130e1438628a
Abhishek Singh <singh.abhishek151019@gmail.com>
2026-08-18 22:34:54 +0530

    Refactor: Add initial implementation of pandas examples for Series and DataFrames in 17_pandas.py

---
+++ python/17_pandas.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+## SERIES
+# x = pd.Series([1, 2, 3, 4, 5])
+# print(x)
+# y = pd.Series([1, 2, 3, 4, 5], index=["a", "b", "c", "d", "e"])
+# print(y)
+# z = pd.Series({"a": 1, "b": 2, "c": 3, "d": 4, "e": 5})
+# print(z)
+# a = pd.Series([1,2])
+# b = pd.Series([4,5,6,7])
+# c = a + b
+# print(c)
+
+## DATAFRAMES
+# a = [1,2,3]
+# b = [4,5,6]
+# c = [7,8,9]
+# d = [10,11,12]
+# df = pd.DataFrame({"A": a, "B": b, "C": c, "D": d}, index=[1,2,3])
+# print(df)
+# print(df.columns)
+# print(df.index)
+# print(df.values)
+# print(df.head(2))
+# print(df.tail(2))
+# print(df["A"][:2])
+# print(df.loc[2]) # loc = location index
+# print(df.iloc[2]) # iloc = integer location
+
+## ARITHMETIC OPERATIONS
+df = pd.DataFrame({"A": [1,2,3], "B": [4,5,6], "C": [7,8,9]}) 
+print(df)
+print("".center(50, "-"))
+df["SUM"] = df["A"] + df["B"] + df["C"]
+df["SUB"] = df["A"] - df["B"] - df["C"]
+df["MUL"] = df["A"] * df["B"] * df["C"]
+df["DIV"] = df["A"] / df["B"] / df["C"]
+df["AgtB"] = df["A"] > df["B"]
+df["AgtC"] = df["A"] > df["C"]
+print(df)
